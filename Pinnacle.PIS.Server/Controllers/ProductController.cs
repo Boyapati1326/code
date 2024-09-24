51d8fea22f6d327af7b99a7e4267af3ec23f2c61
Boyapati1326 <boyapati1326@gmail.com>
2024-09-23 22:41:46 -0400

    Add files via upload

---
+++ Pinnacle.PIS.Server/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pinnacle.PIS.Server.Domain;
+using Pinnacle.PIS.Server.Services.ProductService;
+
+namespace Pinnacle.PIS.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        private ILogger<ProductController> _logger;
+        public ProductController(IProductService productService, ILogger<ProductController> logger)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+        [HttpPost]
+        public async Task<bool> loadNewBatchAsync(List<ImportedData> productInfos)
+        {
+            return await _productService.LoadNewBatchAsync(productInfos);
+        }
+    }
+}
---
+++ Pinnacle.PIS.Server/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Pinnacle.PIS.Repository;
+using Pinnacle.PIS.Server.Services;
+using Pinnacle.PIS.Server.Services.ProductService;
+
+namespace Pinnacle.PIS.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddTransient<PISEntities>();
+            services.AddTransient<IPdfFileHandler, PdfFileHandler>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseRouting();
+            app.UseSwaggerUI();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
